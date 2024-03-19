a5bd3ecca6528c10161bb1d624447408ac0fa28f
Endermandels <elijahdelavar@gmail.com>
2024-03-19 14:49:27 -0700

    Handle indexing or compressing via command line

--- mybitmap.py
+++ mybitmap.py
@@ -35,8 +35,6 @@ def parse_row(row):
 	return parsed + '\n'
 
 def create_index(input_file, output_path, sorted):
-	print('received: ' + input_file + " " + output_path + " " + str(sorted))
-
 	output_file = ''
 	fn = input_file.split('/')[-1]
 
@@ -44,7 +42,6 @@ def create_index(input_file, output_path, sorted):
 		output_file = f"{output_path}/{fn}_sorted"
 	else:
 		output_file = f"{output_path}/{fn}"
-		print(output_file)
 
 	try:
 		with open(input_file, 'r') as infile:
@@ -63,24 +60,56 @@ def create_index(input_file, output_path, sorted):
 		print(f"Error while trying to read from '{input_file}':", e)
 		sys.exit(1)
 
+def compress_index(bitmap_index, output_path, compression_method, word_size):
+	COLUMNS = 16 # Total number of columns in bitmap
+	output_file = ''
+	fn = bitmap_index.split('/')[-1]
+	output_file = f"{output_path}/{fn}_{compression_method}_{str(word_size)}"
 
+	print(output_file)
 
 def main():
-	if len(sys.argv) != 4:
-		print('usage: python3 mybitmap.py <input_file> <output_path> <sorted>')
+	argc = len(sys.argv)
+	if argc < 2:
+		print('usage: python3 mybitmap.py <index> <input_file> <output_path> <sorted>')
+		print('usage: python3 mybitmap.py <compress> <input_file> <output_path> ' \
+			+ '<compression_method> <word_size>')
 		sys.exit(1)
 
-	is_sorted = sys.argv[3].lower()
+	i_or_c = sys.argv[1].lower()
 
-	if is_sorted == 't':
-		SORTED = '_sorted'
-	elif is_sorted != 'f':
-		print("sorted should be either 't' or 'f'")
+	if i_or_c == 'i' or i_or_c == 'index':
+		if argc != 5:
+			print('usage: python3 mybitmap.py <index> <input_file> <output_path> ' \
+				+ '<sorted>')
+			sys.exit(1)
+
+		is_sorted = sys.argv[4].lower()
+
+		if is_sorted == 't':
+			SORTED = '_sorted'
+		elif is_sorted != 'f':
+			print("sorted should be either 't' or 'f'")
+			sys.exit(1)
+
+		create_index(	sys.argv[2],
+				sys.argv[3],
+				is_sorted == 't')
+
+	elif i_or_c == 'c' or i_or_c == 'compress':
+		if argc != 6:
+			print('usage: python3 mybitmap.py <compress> <input_file> <output_path> ' \
+				+ '<compression_method> <word_size>')
+			sys.exit(1)
+
+		compress_index(	sys.argv[2],
+				sys.argv[3],
+				sys.argv[4],
+				int(sys.argv[5]))
+	else:
+		print("Error: Unspecified Index or Compression: Specify by typing 'index' or " \
+			+ "'compression'")
 		sys.exit(1)
-
-	create_index(	sys.argv[1], 
-					sys.argv[2],
-					is_sorted == 't')
 
 if __name__ == '__main__':
 	main()
