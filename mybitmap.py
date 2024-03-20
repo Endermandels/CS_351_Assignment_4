521d4a60e4e62c21a8b47584f0f3def355287095
Endermandels <elijahdelavar@gmail.com>
2024-03-19 18:07:44 -0700

    Compress small bitmap

--- mybitmap.py
+++ mybitmap.py
@@ -60,13 +60,127 @@ def create_index(input_file, output_path, sorted):
 		print(f"Error while trying to read from '{input_file}':", e)
 		sys.exit(1)
 
+def flush_buckets(buckets, word_size):
+	"""
+	Transfer any runs to the string to output.
+	"""
+	for bucket in buckets:
+		if bucket[0]['1'] > 0:
+			# Write run of 1's to file, then store run of 0's
+			bin_str = '{0:b}'.format(bucket[0]['1'])
+			bucket[2] += '11' + '0'*(word_size-2-len(bin_str)) + bin_str
+			bucket[0]['1'] = 0
+
+		elif bucket[0]['0'] > 0:
+			# Write run of 0's to file, then store run of 1's
+			bin_str = '{0:b}'.format(bucket[0]['0'])
+			bucket[2] += '10' + '0'*(word_size-2-len(bin_str)) + bin_str
+			bucket[0]['0'] = 0
+
+
+def modify_buckets(buckets, word_size):
+	"""
+	Buckets keep track of each column's runs/literals.
+	The first value in each bucket indicates how many runs of which bit have been encountered.
+	The next <word_size>-1 values are the encountered bits.
+	When <word_size>-1 values have been read in, check if the stored bits are a literal.
+		If it is, check if the first value is a run, and write it to file if so.
+	Write the literal to file.
+	Empty bucket (after first value).
+	"""
+	for bucket in buckets:
+		run_0 = bucket[1]['0']
+		run_1 = bucket[1]['1']
+
+		if run_0:
+			if bucket[0]['1'] > 0:
+				# Write run of 1's to file, then store run of 0's
+				bin_str = '{0:b}'.format(bucket[0]['1'])
+				bucket[2] += '11' + '0'*(word_size-2-len(bin_str)) + bin_str
+				bucket[0]['1'] = 0
+			bucket[0]['0'] += 1
+
+		elif run_1:
+			if bucket[0]['0'] > 0:
+				# Write run of 0's to file, then store run of 1's
+				bin_str = '{0:b}'.format(bucket[0]['0'])
+				bucket[2] += '10' + '0'*(word_size-2-len(bin_str)) + bin_str
+				bucket[0]['0'] = 0
+			bucket[0]['1'] += 1
+
+		else:
+			# Encountered Literal
+			if bucket[0]['1'] > 0:
+				# Write run of 1's to file, then store run of 0's
+				bin_str = '{0:b}'.format(bucket[0]['1'])
+				bucket[2] += '11' + '0'*(word_size-2-len(bin_str)) + bin_str
+				bucket[0]['1'] = 0
+
+			elif bucket[0]['0'] > 0:
+				# Write run of 0's to file, then store run of 1's
+				bin_str = '{0:b}'.format(bucket[0]['0'])
+				bucket[2] += '10' + '0'*(word_size-2-len(bin_str)) + bin_str
+				bucket[0]['0'] = 0
+
+			bucket[2] += '0' + bucket[3]
+
+		bucket[3] = ''
+		bucket[1]['0'] = 1
+		bucket[1]['1'] = 1
+
 def compress_index(bitmap_index, output_path, compression_method, word_size):
 	COLUMNS = 16 # Total number of columns in bitmap
 	output_file = ''
 	fn = bitmap_index.split('/')[-1]
 	output_file = f"{output_path}/{fn}_{compression_method}_{str(word_size)}"
 
-	print(output_file)
+	# First dict: number and type of runs
+	# Second dict: whether the bits read in is a run and what type they are
+	# First string: bits to output
+	# Second string: bits read in
+	buckets = []
+	for i in range(COLUMNS):
+		buckets.append([{'0': 0, '1': 0}, {'0': 1, '1': 1}, '', ''])
+
+	try:
+		with open(bitmap_index, 'r') as infile:
+			try:
+				with open(output_file, 'w') as outfile:
+
+					rows_read = 0
+					bindex = 0
+					bit = infile.read(1)
+					while bit:
+						if bit == '0':
+							# Add bit to correct column
+							buckets[bindex][1]['1'] = 0
+							buckets[bindex][3] += bit
+							bindex += 1
+						elif bit == '1':
+							# Add bit to correct column
+							buckets[bindex][1]['0'] = 0
+							buckets[bindex][3] += bit
+							bindex += 1
+						else:
+							rows_read += 1
+							if rows_read == word_size-1:
+								modify_buckets(buckets, word_size)
+								rows_read = 0
+							bindex = 0
+
+						bit = infile.read(1)
+
+					flush_buckets(buckets, word_size)
+
+					for bucket in buckets:
+						outfile.write(bucket[2] + '\n')
+
+			except Exception as e:
+				print(f"Error while trying to write to '{output_file}':", e)
+				sys.exit(1)
+	except Exception as e:
+		print(f"Error while trying to read from '{bitmap_index}':", e)
+		sys.exit(1)
 
 def main():
 	argc = len(sys.argv)
